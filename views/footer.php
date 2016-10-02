c3feac59994631d3e0d4da381500593c7c10d21f
Yogesh K Singh <yogesh696ksingh@gmail.com>
2016-10-02 09:05:18 +0530

    login signup modal with category page and compare page front end dev

--- views/footer.php
+++ views/footer.php
@@ -1,27 +1,27 @@
-<div class="callout large secondary">
-      <div class="row">
-        <div class="large-4 columns">
+<div class="callout large secondary"> <!-- footer opening -->
+      <div class="row"> <!-- row for footer -->
+        <div class="large-4 columns"> <!-- left footer -->
           <h5>About The Site</h5>
           <p>All the latest products and comparison for each of them available on this site. Fill in your cart with as many exciting products. Signup and accept the license and enjoy your shopping experience with Know-Ur-Tech</p>
-        </div>
-        <div class="large-3 large-offset-2 columns">
-          <ul class="menu vertical">
+        </div> <!-- end of left footer -->
+        <div class="large-3 large-offset-2 columns"> <!-- right left footer -->
+          <ul class="menu vertical"> <!-- right left footer list -->
             <li><a href="#">Login</a></li>
             <li><a href="#">Signup</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">2014-16 &copy; KuT Technologies Pvt. Ltd. All rights reserved</a></li>
-          </ul>
-        </div>
-        <div class="large-3 columns">
-          <ul class="menu vertical">
+          </ul> <!-- end of right left footer list -->
+        </div> <!-- end of right left footer -->
+        <div class="large-3 columns"> <!-- right right footer -->
+          <ul class="menu vertical"> <!-- right right footer list -->
             <li><a href="#"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
             <li><a href="#"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a></li>
             <li><a href="#">Terms</a></li>
             <li><a href="#">Privacy</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+          </ul> <!-- end of right right footer list -->
+        </div> <!-- end of right right footer -->
+      </div> <!-- closing row for footer -->
+    </div> <!-- closing footer -->
    <!--  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script> -->
     <script src="../library/foundation/js/vendor/jquery.js"></script>
